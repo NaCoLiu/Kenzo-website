23dfcba2caf91286b343de9faa7d83f0ef8f23a2
NaCo <naco@nco.im>
2023-02-11 07:50:01 +0800

    feature(update): bugfix

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import './style.scss'
 import App from './App.vue'
-
+import { AnimateInViewDirective } from "@neeravp/vue-3-animate-in-view";
 const app = createApp(App);
+app.directive("animate-inview", AnimateInViewDirective);
 
 app.mount('#app')
