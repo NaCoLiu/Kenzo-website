b041382902f738039dd699db7ee1adf271000316
NaCo <naco@nco.im>
2023-02-11 08:25:38 +0800

    feature(bugfix): change discord link

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -23,7 +23,7 @@ export const websiteConfig = {
     ],
     banner: {
       title: "Anti-Aim made the right way.",
-      desc: 'At Kenzo, we aim to create the best possible experience. Our LUAs will help you in every aspect, to guarantee the best gameplay possible.',
+      desc: 'At Kenzo, we aim to create the best possible experience. Our LUAs will help you in every aspect, to guarantee the best gameplay.',
       customersNumber: 1000,
       missHeadNumber: 10,
       buttonName: 'Purchase',
